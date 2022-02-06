3d97115e3d86cbe731f2cfc4a6fcfcb40acc0b02
alanfoster <alan@alanfoster.me>
2022-02-06 06:08:51 +0000

    Mettle

--- spec/allure_config.rb
+++ spec/allure_config.rb
@@ -13,12 +13,20 @@ AllureRspec.configure do |config|
 
   # additional metadata
   # environment.properties
-  config.environment_properties = {
+  environment_properties = {
     host_os: RbConfig::CONFIG['host_os'],
-    ruby_version: RUBY_VERSION,
-    meterpreter_name: ENV['METERPRETER'],
-    meterpreter_runtime_version: ['METERPRETER_RUNTIME_VERSION']
+    ruby_version: RUBY_VERSION
   }.compact
+  meterpreter_name = ENV['METERPRETER']
+  meterpreter_runtime_version = ENV['METERPRETER_RUNTIME_VERSION']
+  if meterpreter_name.present?
+    environment_properties[:meterpreter_name] = meterpreter_name
+    if meterpreter_runtime_version.present?
+      environment_properties[:meterpreter_runtime_version] = "#{meterpreter_name}#{meterpreter_runtime_version}"
+    end
+  end
+
+  config.environment_properties = environment_properties.compact
   # categories.json
   # config.categories = File.new("my_custom_categories.json")
 end
